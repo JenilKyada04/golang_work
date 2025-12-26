c9bc14321cfb10b786ab59717dcb0ba5919759c5
Jenil Kyada <jenilkyada007@gmail.com>
2025-12-26 19:07:54 +0530

    Implement fibonacci function with goroutine
    Added a fibonacci function that sends results to a channel.

--- video/Concurrency/main.go
+++ video/Concurrency/main.go
@@ -4,6 +4,21 @@ import "fmt"
 
 // go doSomething()
 
+func febonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
 func main() {
-	fmt.Println("DONE")
+	fmt.Println("Concurrency in Go")
+
+	c := make(chan int)
+	go febonacci(10, c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
